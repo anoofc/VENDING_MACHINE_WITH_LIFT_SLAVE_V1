bf33a37524f1f2c2088886796f9ae5bff50ed361
Anoof Chappangathil <anoof7262@gmail.com>
2024-11-13 11:10:29 +0400

    V1 FINAL

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
-#define DEBUG             1
+#define DEBUG             1       // DON'T CHANGE
 
 #define BAUDRATE          115200
 
-#define HOMING_SPEED      50
-#define MAX_MOVING_SPEED  255
-#define MIN_MOVING_SPEED  10
-#define HOMING_OFFSET     45
+#define HOMING_SPEED      30
+#define MOVING_DOWN_SPEED 80
+#define MAX_MOVING_SPEED  100
+#define MIN_MOVING_SPEED  30
+#define HOMING_OFFSET     100
 
 #define ACCL_END_POS      255
 #define DECL_END_POS      255
@@ -29,12 +30,12 @@
 #define UP_LIMIT_DETECT   digitalRead(LIMIT_UP)   == 0
 #define DOWN_LIMIT_DETECT digitalRead(LIMIT_DOWN) == 0
 
-#define TRAY_1_POSITION   10000
-#define TRAY_2_POSITION   13600
-#define TRAY_3_POSITION   10720
-#define TRAY_4_POSITION   8480
-#define TRAY_5_POSITION   6240
-#define TRAY_6_POSITION   4000
+#define TRAY_1_POSITION   1635
+#define TRAY_2_POSITION   1415
+#define TRAY_3_POSITION   1205
+#define TRAY_4_POSITION   1015
+#define TRAY_5_POSITION   810
+#define TRAY_6_POSITION   600
 
 #define PICKUP_POSITION   0
 
@@ -62,6 +63,8 @@ uint32_t  encoder_r_counter  = 0;
 uint32_t  targetPosition     = 0;
 uint8_t   movingSpeed        = 0;
 
+String    feedbackData = "";
+
 void readEncoder() { 
   currentState = digitalRead(ENCODER_L_PIN);
   if (currentState != lastState){     
@@ -69,60 +72,17 @@ void readEncoder() {
       encoder_l_counter++; encoder_r_counter =0; 
       if (encoder_l_counter > SENSITIVITY){ 
         encoder_l_counter=0; position--; 
-        if (DEBUG){ Serial.println(position); COMMAND_FOR_LEFT; }
+        if (DEBUG){ Serial.println(position);}
       } 
     } else {
       encoder_r_counter++; encoder_l_counter = 0; 
       if(encoder_r_counter > SENSITIVITY) { 
         encoder_r_counter =0; position++; 
-        if (DEBUG) {Serial.println(position); COMMAND_FOR_RIGHT;} 
+        if (DEBUG) {Serial.println(position);} 
       }
     }
   }
   lastState = currentState; 
-}
-
-
-void moveUp() {
-  if (position < targetPosition && !UP_LIMIT_DETECT) {
-    if (position < 255) {
-      analogWrite(MOTOR_UP_PIN, movingSpeed);
-      if (movingSpeed < MAX_MOVING_SPEED) { movingSpeed++; }
-      delay(3);
-    } else if (targetPosition - position < DECL_END_POS) {
-      analogWrite(MOTOR_UP_PIN, movingSpeed);
-      if (movingSpeed > MIN_MOVING_SPEED) { movingSpeed--; }
-      delay(3);
-    }
-  } if (position == targetPosition || UP_LIMIT_DETECT) {
-    analogWrite(MOTOR_UP_PIN, 0);
-    movingSpeed = 0;
-  }
-}
-
-void moveDown() {
-  if (position > targetPosition && !DOWN_LIMIT_DETECT) {
-    if (position > 0) {
-      analogWrite(MOTOR_DOWN_PIN, movingSpeed);
-      if (movingSpeed < MAX_MOVING_SPEED) { movingSpeed++; }
-      delay(3);
-    } else if (position - targetPosition < DECL_END_POS) {
-      analogWrite(MOTOR_DOWN_PIN, movingSpeed);
-      if (movingSpeed > MIN_MOVING_SPEED) { movingSpeed--; }
-      delay(3);
-    }
-  } if (position == targetPosition || DOWN_LIMIT_DETECT) {
-    analogWrite(MOTOR_DOWN_PIN, 0);
-    movingSpeed = 0;
-  }
-}
-
-void readSerial2() {
-  if (Serial2.available()) {
-    String incoming = Serial2.readString();
-    incoming.trim();
-    if (DEBUG) { Serial.println(incoming); SerialBT.println(incoming); }   
-  }
 }
 
 void debugIO() {
@@ -130,19 +90,44 @@ void debugIO() {
 }
 
 void processData(char data){
-  if (data == 'A'){ targetPosition = TRAY_1_POSITION; }
-  if (data == 'B'){ targetPosition = TRAY_2_POSITION; }
-  if (data == 'C'){ targetPosition = TRAY_3_POSITION; }
-  if (data == 'D'){ targetPosition = TRAY_4_POSITION; }
-  if (data == 'E'){ targetPosition = TRAY_5_POSITION; }
-  if (data == 'F'){ targetPosition = TRAY_6_POSITION; }
-  if (data == 'Z'){ targetPosition = PICKUP_POSITION; }
+  if (data == 'A'){ targetPosition = TRAY_1_POSITION; moveUpStatus   = 1; }
+  if (data == 'B'){ targetPosition = TRAY_2_POSITION; moveUpStatus   = 1; }
+  if (data == 'C'){ targetPosition = TRAY_3_POSITION; moveUpStatus   = 1; }
+  if (data == 'D'){ targetPosition = TRAY_4_POSITION; moveUpStatus   = 1; }
+  if (data == 'E'){ targetPosition = TRAY_5_POSITION; moveUpStatus   = 1; }
+  if (data == 'F'){ targetPosition = TRAY_6_POSITION; moveUpStatus   = 1; }
+  if (data == 'Z'){ targetPosition = PICKUP_POSITION; moveDownStatus = 1; }
 }
 
+void processMasterData(String data){
+  if (data.startsWith("*") && data.endsWith("#")){
+    char traySelect = data.charAt(1);
+    if (traySelect == 'A'){ targetPosition = TRAY_1_POSITION; moveUpStatus   = 1; }
+    if (traySelect == 'B'){ targetPosition = TRAY_2_POSITION; moveUpStatus   = 1; }
+    if (traySelect == 'C'){ targetPosition = TRAY_3_POSITION; moveUpStatus   = 1; }
+    if (traySelect == 'D'){ targetPosition = TRAY_4_POSITION; moveUpStatus   = 1; }
+    if (traySelect == 'E'){ targetPosition = TRAY_5_POSITION; moveUpStatus   = 1; }
+    if (traySelect == 'F'){ targetPosition = TRAY_6_POSITION; moveUpStatus   = 1; }
+  } else if (data.startsWith("%") && data.endsWith("#")){
+    char liftDown = data.charAt(1);
+    if (liftDown == 'Z'){ targetPosition = PICKUP_POSITION; moveDownStatus = 1; }
+  }
+}             
 void readSerialBT(){
   if (SerialBT.available()) {
     char incoming = SerialBT.read();
+    SerialBT.print(incoming);
     processData(incoming);  
+  }
+}
+
+void readSerial2() {
+  if (Serial2.available()) {
+    String incoming = Serial2.readString();
+    incoming.trim();
+    feedbackData = incoming;
+    processMasterData(incoming);
+    if (DEBUG) { Serial.println(incoming); SerialBT.println(incoming); }   
   }
 }
 
@@ -151,24 +136,26 @@ void liftHoming() {
   analogWrite(MOTOR_UP_PIN,   0);
   
   if (DEBUG){ Serial.println("Homing...."); }
-  while (digitalRead(LIMIT_DOWN) == 1) { }  // Wait for the limit switch trigger
+  while (digitalRead(LIMIT_DOWN) == 1) { yield(); }  // Wait for the limit switch trigger
   analogWrite(MOTOR_DOWN_PIN, 0);
+  delay(1000);
   position = 0;
   if (DEBUG){ Serial.println("Homing Completed!!"); }
-  delay(1000);
-  analogWrite(MOTOR_UP_PIN,   HOMING_SPEED);
   if (DEBUG){ Serial.println("Setting Offset...."); }
+  analogWrite(MOTOR_UP_PIN,   HOMING_SPEED);
   while (!homingStatus) {
     readEncoder();
-    if (position >= HOMING_OFFSET) { homingStatus = true; }
+    if (position == HOMING_OFFSET) { analogWrite(MOTOR_UP_PIN, 0); homingStatus = true; }
+    yield();
   }
   analogWrite(MOTOR_UP_PIN, 0);
-  if (DEBUG) {Serial.println("Offset Setting Completed!!");}
   delay(1000);
+  if (DEBUG) {Serial.println("Offset Setting Completed!!");}
   position = 0;
 }
 
 void limitCheck () {
+  if (position > TRAY_1_POSITION + 1000){ analogWrite(MOTOR_DOWN_PIN, 0); analogWrite(MOTOR_UP_PIN, 0); position = 0; } 
   if (UP_LIMIT_DETECT) {
     analogWrite(MOTOR_UP_PIN, 0);
     homingStatus = 0;
@@ -177,11 +164,50 @@ void limitCheck () {
   }
   if (DOWN_LIMIT_DETECT) {
     analogWrite(MOTOR_DOWN_PIN, 0);
-    delay (1000);
-    analogWrite(MOTOR_UP_PIN, HOMING_SPEED);
-    delay (3000);
-    movingSpeed = 0;
+    analogWrite(MOTOR_UP_PIN, 0);
+    homingStatus = 0;
     liftHoming();
+  }
+}
+
+void moveUp() {
+  if (position < targetPosition && !UP_LIMIT_DETECT) {
+    if (position < 255) {
+      analogWrite(MOTOR_UP_PIN, movingSpeed);
+      analogWrite(MOTOR_DOWN_PIN, 0);
+      if (movingSpeed < MAX_MOVING_SPEED) { movingSpeed++; }
+      delay(3);
+    } else if (targetPosition - position < 250) {
+      analogWrite(MOTOR_UP_PIN, movingSpeed);
+      analogWrite(MOTOR_DOWN_PIN, 0);
+      if (movingSpeed > HOMING_SPEED) { movingSpeed--; }
+    }
+  } if (position == targetPosition || UP_LIMIT_DETECT) {
+    analogWrite(MOTOR_UP_PIN, 0);
+    analogWrite(MOTOR_DOWN_PIN, 0);
+    moveUpStatus = 0;
+    movingSpeed = 0;
+    Serial2.println(feedbackData);
+    feedbackData = "";
+  }
+}
+
+void moveDown() {
+  analogWrite(MOTOR_DOWN_PIN, movingSpeed);
+  analogWrite(MOTOR_UP_PIN, 0);
+  if (movingSpeed < MOVING_DOWN_SPEED) { movingSpeed++; }
+  delay(3);
+  if (DOWN_LIMIT_DETECT) {
+    analogWrite(MOTOR_DOWN_PIN, 0);
+    analogWrite(MOTOR_UP_PIN, 0);
+    delay(100);
+    movingSpeed = 0;
+    moveDownStatus = 0;
+    homingStatus = 0;
+    liftHoming();
+    delay(1000);
+    Serial2.println("$X#");
+    position = 0;
   }
 }
 
@@ -198,7 +224,11 @@ void io_init(){
 
 
 void coreZeroLoop() {
-  readEncoder();
+  if (moveUpStatus)   { moveUp();   }
+  if (moveDownStatus) { moveDown(); }
+  limitCheck();
+  delay(1);
+  yield();
 }
 
 void Task1code( void * pvParameters ){ for(;;){ coreZeroLoop(); yield();} }
@@ -217,7 +247,7 @@ void setup() {
   io_init();
   Serial.begin(9600);
   Serial2.begin(BAUDRATE);
-  SerialBT.begin("VENDING_MACHINE"); //Bluetooth device name
+  SerialBT.begin("VENDING_L1_TEST"); //Bluetooth device name
   dualCoreSetup();
   liftHoming();
 }
@@ -225,8 +255,6 @@ void setup() {
 void loop() {
   readSerial2();
   readSerialBT();
-  if (moveUpStatus)   { moveUp();   }
-  if (moveDownStatus) { moveDown(); }
-  limitCheck();
+  readEncoder();
   // debugIO();
 }
